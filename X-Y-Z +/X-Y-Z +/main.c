d9e985953e6200906de748dfb91fc97972d60e18
Aries0d0f <aries0d0f@gmail.com>
2016-04-13 20:32:58 +0800

    XYZ+

---
+++ X-Y-Z +/X-Y-Z +/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  X-Y-Z +
+//
+//  Created by Aries on 2016/4/13.
+//  Copyright © 2016年 Aries. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
